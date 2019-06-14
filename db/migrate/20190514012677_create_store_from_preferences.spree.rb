1d9e91a6569b8e09dd55ee2f29da6050b2fdf113
Ryan Gale <ryanwfgale@gmail.com>
2019-06-14 17:25:22 -0600

    add changes for client

--- db/migrate/20190514012677_create_store_from_preferences.spree.rb
+++ db/migrate/20190514012677_create_store_from_preferences.spree.rb
@@ -18,13 +18,13 @@ class CreateStoreFromPreferences < ActiveRecord::Migration[4.2]
       # we set defaults for the things we now require
       Spree::Store.new do |s|
         s.name              = preference_store.get 'spree/app_configuration/site_name' do
-          'Spree Demo Site'
+          'Dosi Design Shop'
         end
         s.url               = preference_store.get 'spree/app_configuration/site_url' do
-          'demo.spreecommerce.org'
+          'dosidesigns.com'
         end
         s.mail_from_address = preference_store.get 'spree/app_configuration/mails_from' do
-          'spree@example.com'
+          'Smarthomepro88@gmail.com'
         end
 
         s.meta_description = preference_store.get('spree/app_configuration/default_meta_description') {}
